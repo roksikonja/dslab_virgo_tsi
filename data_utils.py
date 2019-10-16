Update visualizer, constants, data_utils, fix moving average

data_utils.py
@@ -60,15 +60,30 @@ def make_dir(directory):
     return directory
 
 
-def moving_average_std(x, w, center=True):
-    if not isinstance(x, pd.Series):
-        x = pd.Series(x)
+def moving_average_std(x, t, w, center=True):
+    if t.tolist():
+        w = w + 1e-10
+        x_mean = np.zeros(shape=x.shape)
+        x_std = np.zeros(shape=x.shape)
+        for i in range(x.shape[0]):
+            t_center = t[i]
+            window = np.multiply(np.greater_equal(t_center + w, t), np.less_equal(t_center - w, t))
+            slice = x[window]
 
-    if w > 1:
-        x = x.rolling(w, center=center)
-        return x.mean(), x.std()
+            indices = notnan_indices(slice)
+            slice = slice[indices]
+            x_mean[i] = slice.mean()
+            x_std[i] = slice.std()
+        return x_mean, x_std
     else:
-        return x, np.zeros(shape=x.shape)
+        if not isinstance(x, pd.Series):
+            x = pd.Series(x)
+
+        if w > 1:
+            x = x.rolling(w, center=center)
+            return x.mean(), x.std()
+        else:
+            return x, None
 
 
 def get_sampling_intervals(t, x):

modeling_1.py
@@ -1,15 +1,12 @@
 from data_utils import load_data, make_dir, downsample_signal, moving_average_std, notnan_indices, mission_day_to_year
 from constants import Constants as C
 from modeling import compute_exposure, initial_fit, em_estimate_exp_lin, em_estimate_exp, DegradationModels
+from visualizer import Visualizer
 
 import os
 import datetime
 import argparse
-
 import numpy as np
-import matplotlib.pyplot as plt
-from matplotlib import style
-
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--model_type", type=str, default="exp_lin", help="Model to train.")
@@ -17,6 +14,9 @@ parser.add_argument("--visualize", action="store_true", help="Flag for visualizi
 parser.add_argument("--window", type=int, default=81, help="Moving average window size.")
 
 ARGS = parser.parse_args()
+
+visualizer = Visualizer()
+visualizer.set_figsize()
 
 # Parameters
 data_dir = C.DATA_DIR
@@ -30,147 +30,128 @@ make_dir(results_dir)
 # Load data
 data = load_data(os.path.join(data_dir, virgo_file))
 
-t = data["timestamp"]
-pmo_a = data["pmo6v_a"]
-pmo_b = data["pmo6v_b"]
+t = data[C.T].values
+pmo_a = data[C.A].values
+pmo_b = data[C.B].values
 
 # Calculate exposure
-data["exposure_a"] = compute_exposure(pmo_a, "exposure_sum", pmo_b.mean())
-data["exposure_b"] = compute_exposure(pmo_b, "exposure_sum", pmo_b.mean())
-e_a = data["exposure_a"].values
-e_b = data["exposure_b"].values
+data[C.EA] = compute_exposure(pmo_a, "exposure_sum", pmo_b[notnan_indices(pmo_b)].mean())
+data[C.EB] = compute_exposure(pmo_b, "exposure_sum", pmo_b[notnan_indices(pmo_b)].mean())
+e_a = data[C.EA].values
+e_b = data[C.EB].values
 
 # Extract not nan rows
-data_nn = data[["timestamp", "pmo6v_a", "pmo6v_b", "exposure_a", "exposure_b"]].dropna()
-t_nn = data_nn["timestamp"].values
-e_a_nn = data_nn["exposure_a"].values
-e_b_nn = data_nn["exposure_b"].values
-x_a_nn = data_nn["pmo6v_a"].values
-x_b_nn = data_nn["pmo6v_b"].values
+data_nn = data[[C.T, C.A, C.B, C.EA, C.EB]].dropna()
+t_nn = data_nn[C.T].values
+e_a_nn = data_nn[C.EA].values
+e_b_nn = data_nn[C.EB].values
+x_a_nn = data_nn[C.A].values
+x_b_nn = data_nn[C.B].values
 
 # Relative degradation ratio PMO6V-A to PMO6V-B
-ratio_a_b = x_a_nn/x_b_nn
+ratio_a_b = np.divide(x_a_nn, x_b_nn)
 
 ratio_a_b_initial = None
 ratio_a_b_c = None
 x_a_nn_c = None
 x_b_nn_c = None
 parameters_opt = None
+MODEL = None
+WINDOW = ARGS.window
 
-
-if ARGS.model_type == "exp_lin":
+if ARGS.model_type.upper() == C.EXP_LIN:
+    MODEL = C.EXP_LIN
     # Initial parameter estimation
     gamma_initial, lambda_initial, e_0_initial = initial_fit(ratio_a_b, e_a_nn)
     ratio_a_b_initial = DegradationModels.exp_unc_model(e_a_nn, gamma_initial, lambda_initial, e_0_initial)
 
     parameters_initial = [gamma_initial, lambda_initial, e_0_initial, 0]
     parameters_opt, x_a_nn_c, x_b_nn_c = em_estimate_exp_lin(x_a_nn, x_b_nn, e_a_nn, e_b_nn, parameters_initial)
-    ratio_a_b_c = x_a_nn_c / x_b_nn_c
+    ratio_a_b_c = np.divide(x_a_nn_c, x_b_nn_c)
 
-elif ARGS.model_type == "exp":
+elif ARGS.model_type.upper() == C.EXP:
+    MODEL = C.EXP
     gamma_initial, lambda_initial, e_0_initial = initial_fit(ratio_a_b, e_a_nn)
     ratio_a_b_initial = DegradationModels.exp_unc_model(e_a_nn, gamma_initial, lambda_initial, e_0_initial)
 
     parameters_initial = [gamma_initial, lambda_initial, e_0_initial, 0]
     parameters_opt, x_a_nn_c, x_b_nn_c = em_estimate_exp(x_a_nn, x_b_nn, e_a_nn, e_b_nn, parameters_initial)
-    ratio_a_b_c = x_a_nn_c / x_b_nn_c
+    ratio_a_b_c = np.divide(x_a_nn_c, x_b_nn_c)
 
 
 # Results
 k_a = 10000
-x_a = pmo_a.values
+x_a = pmo_a
 idx_a = notnan_indices(x_a)
 x_a = downsample_signal(x_a[idx_a], k_a)
 t_a = downsample_signal(t[idx_a], k_a)
 e_a = downsample_signal(e_a[idx_a], k_a)
 
 k_b = 10
-x_b = pmo_b.values
+x_b = pmo_b
 idx_b = notnan_indices(x_b)
 x_b = downsample_signal(x_b[idx_b], k_b)
 t_b = downsample_signal(t[idx_b], k_b)
 e_b = downsample_signal(e_b[idx_b], k_b)
-
-# Mission day to year
-t_nn = np.array(list(map(mission_day_to_year, t_nn)))
-t_a = np.array(list(map(mission_day_to_year, t_a)))
-t_b = np.array(list(map(mission_day_to_year, t_b)))
 
 x_a_c = None
 x_b_c = None
 deg_a = None
 deg_b = None
 
-if ARGS.model_type == "exp_lin":
+if ARGS.model_type.upper() == C.EXP_LIN:
     x_a_c = x_a / DegradationModels.exp_lin_model(e_a, *parameters_opt)
     x_b_c = x_b / DegradationModels.exp_lin_model(e_b, *parameters_opt)
     deg_a = DegradationModels.exp_lin_model(e_a_nn, *parameters_opt)
     deg_b = DegradationModels.exp_lin_model(e_b_nn, *parameters_opt)
 
-elif ARGS.model_type == "exp":
+elif ARGS.model_type.upper() == C.EXP:
     x_a_c = x_a / DegradationModels.exp_model(e_a, *parameters_opt)
     x_b_c = x_b / DegradationModels.exp_model(e_b, *parameters_opt)
     deg_a = DegradationModels.exp_model(e_a_nn, *parameters_opt)
     deg_b = DegradationModels.exp_model(e_b_nn, *parameters_opt)
 
-x_a_ma, x_a_std = moving_average_std(x_a, w=ARGS.window, center=True)
-x_a_c_ma, x_a_c_std = moving_average_std(x_a_c, w=ARGS.window, center=True)
-x_b_ma, x_b_std = moving_average_std(x_b, w=ARGS.window, center=True)
-x_b_c_ma, x_b_c_std = moving_average_std(x_b_c, w=ARGS.window, center=True)
 
-style.use(C.MATPLOTLIB_STYLE)
+figs = []
+fig = visualizer.plot_signals([(t_nn, ratio_a_b, "RATIO_{}_{}_raw".format(C.A, C.B)),
+                               (t_nn, ratio_a_b_initial, "RATIO_{}_{}_initial_fit".format(C.A, C.B))],
+                              results_dir, "RATIO_{}_{}_raw_initial_fit".format(C.A, C.B), x_ticker=1,
+                              legend="upper right", x_label=C.YEAR_UNIT, y_label=C.TSI_UNIT)
+figs.append(fig)
 
-plt.figure(1, figsize=C.FIG_SIZE)
-plt.plot(t_nn, ratio_a_b, t_nn, ratio_a_b_initial)
-plt.title("PMO6V-a to PMO6V-b ratio - raw, initial fit")
-plt.savefig(os.path.join(results_dir, ARGS.model_type + "_ratio_a_b_raw_initial.pdf"),
-            bbox_inches="tight", quality=100, dpi=200)
+fig = visualizer.plot_signals([(t_nn, x_a_nn, "{}_raw_nn".format(C.A)),
+                               (t_nn, x_b_nn, "{}_raw_nn".format(C.B)),
+                               (t_nn, x_a_nn_c, "{}_raw_nn_corrected".format(C.A)),
+                               (t_nn, x_b_nn_c, "{}_raw_nn_corrected".format(C.B))],
+                              results_dir, "{}_{}_{}_raw_corrected".format(MODEL, C.A, C.B), x_ticker=1,
+                              legend="upper right", x_label=C.YEAR_UNIT, y_label=C.TSI_UNIT)
+figs.append(fig)
 
-plt.figure(2, figsize=C.FIG_SIZE)
-plt.plot(t_nn, x_b_nn, t_nn, x_a_nn_c, t_nn, x_b_nn_c)
-plt.legend(["pmo_b", "pmo_a_c", "pmo_b_c"])
-plt.title("PMO6V-a and PMO6V-b - raw, degradation corrected")
-plt.savefig(os.path.join(results_dir, ARGS.model_type + "_pmo_a_b_c.pdf"),
-            bbox_inches="tight", quality=100, dpi=200)
+fig = visualizer.plot_signals([(t_nn, ratio_a_b, "RATIO_{}_{}_raw".format(C.A, C.B)),
+                               (t_nn, ratio_a_b_c, "RATIO_{}_{}_corrected".format(C.A, C.B)),
+                               (t_nn, deg_a, "DEGRADATION_{}".format(C.A)),
+                               (t_nn, deg_b, "DEGRADATION_{}".format(C.B))],
+                              results_dir, "{}_RATIO_DEGRADATION_{}_{}_raw_corrected".format(MODEL, C.A, C.B),
+                              x_ticker=1, legend="upper right", x_label=C.YEAR_UNIT, y_label=C.TSI_UNIT)
+figs.append(fig)
 
-plt.figure(3, figsize=C.FIG_SIZE)
-plt.plot(t_nn, ratio_a_b, t_nn, ratio_a_b_c, t_nn, deg_a, t_nn, deg_b)
-plt.title("PMO6V-a to PMO6V-b ratio - raw, degradation corrected")
-plt.legend(["ratio_a_b_raw", "ratio_a_b_c", "deg_a_opt", "deg_b_opt"])
-plt.savefig(os.path.join(results_dir,  ARGS.model_type + "_ratio_a_b_raw_opt.pdf"),
-            bbox_inches="tight", quality=100, dpi=200)
+fig = visualizer.plot_signals([(t_a, x_a, "{}_raw".format(C.A)),
+                               (t_b, x_b, "{}_raw".format(C.B)),
+                               (t_a, x_a_c, "{}_raw_corrected".format(C.A)),
+                               (t_b, x_b_c, "{}_raw_corrected".format(C.B))],
+                              results_dir, "{}_{}_{}_raw_corrected_full".format(MODEL, C.A, C.B), x_ticker=1,
+                              legend="upper right", x_label=C.YEAR_UNIT, y_label=C.TSI_UNIT)
+figs.append(fig)
 
-plt.figure(4, figsize=C.FIG_SIZE)
-plt.scatter(t_a, x_a, marker="x", c="b")
-plt.scatter(t_b, x_b, marker="x", c="r")
-plt.plot(t_a, x_a_c)
-plt.plot(t_b, x_b_c)
-plt.title("PMO6V-a and PMO6V-b - raw, degradation corrected")
-plt.legend(["pmo_a", "pmo_b", "pmo_a_c", "pmo_b_c"], loc="lower left")
-plt.savefig(os.path.join(results_dir,  ARGS.model_type + "_pmo_a_b_c_full.pdf"),
-            bbox_inches="tight", quality=100, dpi=200)
+fig = visualizer.plot_signals_mean_std([(t_a, x_a, "{}_conf_int".format(C.A), WINDOW),
+                                        (t_b, x_b, "{}_conf_int".format(C.B), WINDOW),
+                                        (t_a, x_a_c, "{}_corrected_conf_int".format(C.A), WINDOW),
+                                        (t_b, x_b_c, "{}_corrected_conf_int".format(C.B), WINDOW)],
+                                       results_dir, "{}_{}_{}_raw_corrected_full_conf_int".format(MODEL, C.A, C.B),
+                                       x_ticker=1, legend="lower left", x_label=C.YEAR_UNIT, y_label=C.TSI_UNIT)
+figs.append(fig)
 
-plt.figure(5, figsize=C.FIG_SIZE)
-plt.plot(t_a, x_a_ma, color="tab:blue", lw=0.5)
-plt.fill_between(t_a, x_a_ma - 1.96 * x_a_std, x_a_ma + 1.96 * x_a_std,
-                 facecolor='tab:blue', alpha=0.5, label='95% confidence interval')
-
-plt.plot(t_b, x_b_ma, color="tab:orange", lw=0.5)
-plt.fill_between(t_b, x_b_ma - 1.96 * x_b_std, x_b_ma + 1.96 * x_b_std,
-                 facecolor='tab:orange', alpha=0.5, label='95% confidence interval')
-
-plt.plot(t_a, x_a_c_ma, color="tab:green", lw=0.5)
-plt.fill_between(t_a, x_a_c_ma - 1.96 * x_a_c_std, x_a_c_ma + 1.96 * x_a_c_std,
-                 facecolor='tab:green', alpha=0.5, label='95% confidence interval')
-
-plt.plot(t_b, x_b_c_ma, color="tab:red", lw=0.5)
-plt.fill_between(t_b, x_b_c_ma - 1.96 * x_b_c_std, x_b_c_ma + 1.96 * x_b_c_std,
-                 facecolor='tab:red', alpha=0.5, label='95% confidence interval')
-
-plt.title("PMO6V-a and PMO6V-b - raw, degradation corrected, moving average")
-plt.legend(["pmo_a_ma", "pmo_b_ma", "pmo_a_c_ma", "pmo_b_c_ma"], loc="lower left")
-plt.savefig(os.path.join(results_dir,  ARGS.model_type + "_pmo_a_b_c_full_ma.pdf"),
-            bbox_inches="tight", quality=100, dpi=200)
 
 if ARGS.visualize:
-    plt.show()
+    for fig in figs:
+        fig.show()

visualizer.py
@@ -1,9 +1,12 @@
 from constants import Constants as C
+from data_utils import moving_average_std, mission_day_to_year
 
 import matplotlib.pyplot as plt
 from matplotlib import style
 import matplotlib as mpl
 import matplotlib.ticker as ticker
+from scipy.stats import norm
+import numpy as np
 
 import os
 
@@ -22,14 +25,17 @@ class Visualizer(object):
         mpl.rcParams['figure.figsize'] = list(size)
 
     @staticmethod
-    def plot_signals(signal_triplets, results_dir, title, x_ticker=None, legend=None, y_lim=None,
+    def plot_signals(signal_threeplets, results_dir, title, x_ticker=None, legend=None, y_lim=None,
                      x_label=None, y_label=None):
 
         fig = plt.figure()
-        for signal_triplet in signal_triplets:
-            t = signal_triplet[0]
-            x = signal_triplet[1]
-            label = signal_triplet[2]
+        for signal_threeplet in signal_threeplets:
+            t = signal_threeplet[0]
+            if x_label == C.YEAR_UNIT:
+                t = np.array(list(map(mission_day_to_year, t)))
+
+            x = signal_threeplet[1]
+            label = signal_threeplet[2]
             plt.plot(t, x, label=label)
 
         plt.title(title)
@@ -53,4 +59,55 @@ class Visualizer(object):
 
         if results_dir:
             plt.savefig(os.path.join(results_dir, title))
+        return fig
+
+    @staticmethod
+    def plot_signals_mean_std(signal_fourplets, results_dir, title, x_ticker=None, legend=None, y_lim=None,
+                              x_label=None, y_label=None, confidence=0.95, alpha=0.5):
+
+        fig = plt.figure()
+        for signal_fourplet in signal_fourplets:
+            factor = norm.ppf(confidence) - norm.ppf(1 - confidence)
+            t = signal_fourplet[0]
+
+            x = signal_fourplet[1]
+            label = signal_fourplet[2]
+
+            if signal_fourplet[3]:
+                x_ma, x_std = moving_average_std(x, t, w=signal_fourplet[3], center=True)
+
+                if x_label == C.YEAR_UNIT:
+                    t = np.array(list(map(mission_day_to_year, t)))
+
+                plt.plot(t, x_ma, label=label)
+                plt.fill_between(t, x_ma - factor * x_std, x_ma + factor * x_std, alpha=alpha,
+                                 label='{}_{}_conf_interval'.format(label, confidence))
+            else:
+                if x_label == C.YEAR_UNIT:
+                    t = np.array(list(map(mission_day_to_year, t)))
+
+                plt.plot(t, x, label=label)
+
+        plt.title(title)
+
+        if x_ticker:
+            plt.gca().xaxis.set_major_locator(ticker.MultipleLocator(x_ticker))
+
+        if legend:
+            plt.legend(loc=legend)
+        else:
+            plt.legend()
+
+        if y_lim:
+            plt.ylim(y_lim)
+
+        if x_label:
+            plt.xlabel(x_label)
+
+        if y_label:
+            plt.ylabel(y_label)
+
+        if results_dir:
+            plt.savefig(os.path.join(results_dir, title))
+
         return fig