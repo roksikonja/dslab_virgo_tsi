7343fa3ee2d78cc39e979e5ac27ed71b459abba4
Luka Kolar <kolarluka96@gmail.com>
2019-10-21 23:19:38 +0200

    Consistent variable naming

--- run_modeling.py
+++ run_modeling.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 
 from dslab_virgo_tsi.constants import Constants as Const
 from dslab_virgo_tsi.data_utils import load_data, make_dir
-from dslab_virgo_tsi.models import ExposureMode, ExpModel, ExpLinModel, ModelingResult
+from dslab_virgo_tsi.models import ExposureMode, ExpModel, ExpLinModel, ModelingResult, IterationResult
 from dslab_virgo_tsi.visualizer import Visualizer
 
 
@@ -39,75 +39,72 @@ def load_modeling_result(results_dir, model_name):
 
 
 def plot_results(results: ModelingResult, results_dir, model_name, window_size):
+    first_iteration: IterationResult = results.history_mutual_nn[0]
+    last_iteration: IterationResult = results.history_mutual_nn[-1]
+
     print("plotting results ...")
     figs = []
-    fig = Visualizer.plot_signals([(results.t_mutual_nn, results.history_mutual_nn[0].iteration_ratio_a_b,
-                                    "RATIO_{}_{}_raw".format(Const.A, Const.B), False)],
-                                  results_dir, "RATIO_{}_{}_raw_initial_fit".format(Const.A, Const.B), x_ticker=1,
-                                  legend="upper right", x_label=Const.YEAR_UNIT, y_label=Const.TSI_UNIT)
+    fig = visualizer.plot_signals(
+        [(results.t_mutual_nn, first_iteration.ratio_a_b, f"RATIO_{Const.A}_{Const.B}_raw", False)],
+        results_dir, f"RATIO_{Const.A}_{Const.B}_raw_initial_fit", x_ticker=1, legend="upper right",
+        x_label=Const.YEAR_UNIT, y_label=Const.TSI_UNIT)
     figs.append(fig)
 
-    fig = Visualizer.plot_signals(
-        [(results.t_mutual_nn, results.history_mutual_nn[0].iteration_signal_a, "{}_raw_nn".format(Const.A), False),
-         (results.t_mutual_nn, results.history_mutual_nn[0].iteration_signal_b, "{}_raw_nn".format(Const.B), False),
-         (results.t_mutual_nn, results.history_mutual_nn[-1].iteration_signal_a, "{}_raw_nn_corrected".format(Const.A),
-          False),
-         (
-             results.t_mutual_nn, results.history_mutual_nn[-1].iteration_signal_b,
-             "{}_raw_nn_corrected".format(Const.B), False)],
-        results_dir, "{}_{}_{}_raw_corrected".format(model_name, Const.A, Const.B), x_ticker=1,
+    fig = visualizer.plot_signals(
+        [(results.t_mutual_nn, first_iteration.a, f"{Const.A}_raw_nn", False),
+         (results.t_mutual_nn, first_iteration.b, f"{Const.B}_raw_nn", False),
+         (results.t_mutual_nn, last_iteration.a, f"{Const.A}_raw_nn_corrected", False),
+         (results.t_mutual_nn, last_iteration.b, f"{Const.B}_raw_nn_corrected", False)],
+        results_dir, f"{model_name}_{Const.A}_{Const.B}_raw_corrected", x_ticker=1, legend="upper right",
+        x_label=Const.YEAR_UNIT, y_label=Const.TSI_UNIT)
+    figs.append(fig)
+
+    fig = visualizer.plot_signals(
+        [(results.t_mutual_nn, first_iteration.ratio_a_b, f"RATIO_{Const.A}_{Const.B}_raw", False),
+         (results.t_mutual_nn, last_iteration.ratio_a_b, f"RATIO_{Const.A}_{Const.B}_corrected", False)],
+        results_dir, f"{model_name}_RATIO_DEGRADATION_{Const.A}_{Const.B}_raw_corrected", x_ticker=1,
         legend="upper right", x_label=Const.YEAR_UNIT, y_label=Const.TSI_UNIT)
     figs.append(fig)
 
-    fig = Visualizer.plot_signals([(results.t_mutual_nn, results.history_mutual_nn[0].iteration_ratio_a_b,
-                                    "RATIO_{}_{}_raw".format(Const.A, Const.B), False),
-                                   (results.t_mutual_nn, results.history_mutual_nn[-1].iteration_ratio_a_b,
-                                    "RATIO_{}_{}_corrected".format(Const.A, Const.B), False)],
-                                  results_dir,
-                                  "{}_RATIO_DEGRADATION_{}_{}_raw_corrected".format(model_name, Const.A, Const.B),
-                                  x_ticker=1, legend="upper right", x_label=Const.YEAR_UNIT, y_label=Const.TSI_UNIT)
+    fig = visualizer.plot_signals(
+        [(results.t_a_nn, results.a_nn, f"{Const.A}_raw", False),
+         (results.t_b_nn, results.b_nn, f"{Const.B}_raw", False),
+         (results.t_a_nn, results.a_nn_corrected, f"{Const.A}_raw_corrected", False),
+         (results.t_b_nn, results.b_nn_corrected, f"{Const.B}_raw_corrected", False)],
+        results_dir, f"{model_name}_{Const.A}_{Const.B}_raw_corrected_full", x_ticker=1, y_lim=[1357, 1369],
+        legend="upper right", x_label=Const.YEAR_UNIT, y_label=Const.TSI_UNIT)
     figs.append(fig)
 
-    fig = Visualizer.plot_signals([(results.t_a_nn, results.signal_a_nn, "{}_raw".format(Const.A), False),
-                                   (results.t_b_nn, results.signal_b_nn, "{}_raw".format(Const.B), False),
-                                   (results.t_a_nn, results.signal_a_nn_corrected, "{}_raw_corrected".format(Const.A),
-                                   False),
-                                   (results.t_b_nn, results.signal_b_nn_corrected, "{}_raw_corrected".format(Const.B),
-                                    False)
-                                   ],
-                                  results_dir, "{}_{}_{}_raw_corrected_full".format(model_name, Const.A, Const.B),
-                                  x_ticker=1, y_lim=[1357, 1369],
-                                  legend="upper right", x_label=Const.YEAR_UNIT, y_label=Const.TSI_UNIT)
+    """
+    fig = Visualizer.plot_signals_mean_std(
+        [(results.t_a_nn, results.a_nn, f"{Const.A}_conf_int", window_size),
+         (results.t_b_nn, results.b_nn, f"{Const.B}_conf_int", window_size),
+         (results.t_a_nn, results.a_nn_corrected, f"{Const.A}_corrected_conf_int", window_size),
+         (results.t_b_nn, results.b_nn_corrected, f"{Const.B}_corrected_conf_int", window_size)],
+        results_dir, f"{model_name}_{Const.A}_{Const.B}_raw_corrected_full_conf_int", x_ticker=1, legend="lower left", 
+        x_label=Const.YEAR_UNIT, y_label=Const.TSI_UNIT, y_lim=[1357, 1369])
     figs.append(fig)
 
-    # fig = Visualizer.plot_signals_mean_std(
-    #     [(results.t_a_nn, results.signal_a_nn, "{}_conf_int".format(Const.A), window_size),
-    #      (results.t_b_nn, results.signal_b_nn, "{}_conf_int".format(Const.B), window_size),
-    #      (results.t_a_nn, results.signal_a_nn_corrected, "{}_corrected_conf_int".format(Const.A), window_size),
-    #      (results.t_b_nn, results.signal_b_nn_corrected, "{}_corrected_conf_int".format(Const.B), window_size)],
-    #     results_dir, "{}_{}_{}_raw_corrected_full_conf_int".format(model_name, Const.A, Const.B),
-    #     x_ticker=1, legend="lower left", x_label=Const.YEAR_UNIT, y_label=Const.TSI_UNIT, y_lim=[1357, 1369])
-    # figs.append(fig)
+    fig = Visualizer.plot_signals(
+        [(results.t_hourly_out, results.signal_hourly_out, f"TSI_hourly_{model_name}", False)],
+        results_dir, "TSI_hourly_{}".format(model_name), x_ticker=1, legend="upper left", y_lim=[1357, 1369],
+        x_label=Const.YEAR_UNIT, y_label=Const.TSI_UNIT)
+    figs.append(fig)
 
-    # fig = Visualizer.plot_signals(
-    #     [(results.t_hourly_out, results.signal_hourly_out, "TSI_hourly_{}".format(model_name), False)],
-    #     results_dir, "TSI_hourly_{}".format(model_name), x_ticker=1, legend="upper left", y_lim=[1357, 1369],
-    #     x_label=Const.YEAR_UNIT, y_label=Const.TSI_UNIT)
-    # figs.append(fig)
-    #
-    # fig = Visualizer.plot_signals(
-    #     [(results.t_daily_out, results.signal_daily_out, "TSI_daily_{}".format(model_name), False)],
-    #     results_dir, "TSI_daily_{}".format(model_name), x_ticker=1, legend="upper left", y_lim=[1357, 1369],
-    #     x_label=Const.YEAR_UNIT, y_label=Const.TSI_UNIT)
-    # figs.append(fig)
-    #
-    # fig = Visualizer.plot_signals(
-    #     [(results.t_hourly_out, results.signal_hourly_out, "TSI_hourly_{}".format(model_name), False),
-    #      (results.t_a_nn, results.signal_a_nn_corrected, "{}_raw_corrected".format(Const.A), False),
-    #      (results.t_b_nn, results.signal_b_nn_corrected, "{}_raw_corrected".format(Const.B), False)],
-    #     results_dir, "TSI_{}_{}_hourly_{}".format(model_name, Const.A, Const.B), x_ticker=1, legend="upper left",
-    #     x_label=Const.YEAR_UNIT, y_label=Const.TSI_UNIT, y_lim=[1357, 1369])
-    # figs.append(fig)
+    fig = Visualizer.plot_signals(
+        [(results.t_daily_out, results.signal_daily_out, f"TSI_daily_{model_name}", False)],
+        results_dir, "TSI_daily_{}".format(model_name), x_ticker=1, legend="upper left", y_lim=[1357, 1369],
+        x_label=Const.YEAR_UNIT, y_label=Const.TSI_UNIT)
+    figs.append(fig)
+
+    fig = Visualizer.plot_signals(
+        [(results.t_hourly_out, results.signal_hourly_out, f"TSI_hourly_{model_name}", False),
+         (results.t_a_nn, results.a_nn_corrected, f"{Const.A}_raw_corrected", False),
+         (results.t_b_nn, results.b_nn_corrected, f"{Const.B}_raw_corrected", False)],
+        results_dir, "TSI_{}_{}_hourly_{}".format(model_name, Const.A, Const.B), x_ticker=1, legend="upper left",
+        x_label=Const.YEAR_UNIT, y_label=Const.TSI_UNIT, y_lim=[1357, 1369])
+    figs.append(fig)
+    """
 
 
 if __name__ == "__main__":
@@ -117,25 +114,25 @@ if __name__ == "__main__":
     # Load data
     data_pmo6v = load_data(os.path.join(Const.DATA_DIR, Const.VIRGO_FILE))
 
-    Visualizer = Visualizer()
-    Visualizer.set_figsize()
+    visualizer = Visualizer()
+    visualizer.set_figsize()
 
     # Perform modeling
     model = None
     if not ARGS.reuse:
         if ARGS.model_type == "exp_lin":
             model = ExpLinModel(data=data_pmo6v,
-                                timestamp_field_name=Const.T,
-                                signal_a_field_name=Const.A,
-                                signal_b_field_name=Const.B,
+                                t_field_name=Const.T,
+                                a_field_name=Const.A,
+                                b_field_name=Const.B,
                                 exposure_mode=ExposureMode.NUM_MEASUREMENTS,
                                 moving_average_window=ARGS.window,
                                 outlier_fraction=ARGS.outlier_fraction)
         elif ARGS.model_type == "exp":
             model = ExpModel(data=data_pmo6v,
-                             timestamp_field_name=Const.T,
-                             signal_a_field_name=Const.A,
-                             signal_b_field_name=Const.B,
+                             t_field_name=Const.T,
+                             a_field_name=Const.A,
+                             b_field_name=Const.B,
                              exposure_mode=ExposureMode.EXPOSURE_SUM,
                              moving_average_window=ARGS.window,
                              outlier_fraction=ARGS.outlier_fraction)
